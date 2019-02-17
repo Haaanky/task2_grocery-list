160bf29c5d19bb4c43ce10d423da9b60a647b42a
Henrik Norlin Frisemo <haaanky@gmail.com>
2019-02-17 09:33:37 +0100

    2 major changes left

--- Inlämningsuppgift_2/Program.cs
+++ Inlämningsuppgift_2/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 
 namespace Inlämningsuppgift_2
 {
     class Program
     {
-        static Dictionary<int, string> items;
+        static Dictionary<string, int> items;
         private static int enteredItems;
         private static int inputKey;
 
@@ -19,7 +20,7 @@ namespace Inlämningsuppgift_2
         private static void StartGame()
         {
             //init dict
-            items = new Dictionary<int, string>();
+            items = new Dictionary<string, int>();
         }
 
         private static void RunApp()
@@ -35,15 +36,16 @@ namespace Inlämningsuppgift_2
             Console.Clear();
             Console.WriteLine("Grocery list app");
             Console.WriteLine();
-            Console.WriteLine("1. Add item");
-            Console.WriteLine("2. Remove item");
-            Console.WriteLine("3. View list");
-            Console.WriteLine("4. Modify item");
-            Console.WriteLine("5. Check most inexpensive");
-            Console.WriteLine("6. Check most expensive");
-            Console.WriteLine("7. Exit");
+            Console.WriteLine("1. Add item"); //must
+            Console.WriteLine("2. Remove item"); //tbd
+            Console.WriteLine("3. View list"); //done
+            Console.WriteLine("4. Modify item"); //tbd 
+            Console.WriteLine("5. Check most inexpensive"); //must
+            Console.WriteLine("6. Check most expensive"); //must
+            Console.WriteLine("7. Exit"); //done
             Console.WriteLine();
             Console.WriteLine("Press 1 - 7 to choose what to do");
+            Console.WriteLine();
         }
 
         private static bool SelectMenu()
@@ -53,15 +55,10 @@ namespace Inlämningsuppgift_2
             switch (inputKey)
             {
                 case 1:
-                    if (enteredItems == 0)
-                    {
-                        EmptyDict();
-                        return true;
-                    }
-                    AddItem();
+                    AddItems();
                     return true;
                 case 2:
-                    if (enteredItems == 0)
+                    if (items.Count == 0)
                     {
                         EmptyDict();
                         return true;
@@ -69,7 +66,7 @@ namespace Inlämningsuppgift_2
                     RemoveItem();
                     return true;
                 case 3:
-                    if (enteredItems == 0)
+                    if (items.Count == 0)
                     {
                         EmptyDict();
                         return true;
@@ -77,7 +74,7 @@ namespace Inlämningsuppgift_2
                     PrintDict();
                     return true;
                 case 4:
-                    if (enteredItems == 0)
+                    if (items.Count == 0)
                     {
                         EmptyDict();
                         return true;
@@ -85,7 +82,7 @@ namespace Inlämningsuppgift_2
                     ChangeItem();
                     return true;
                 case 5:
-                    if (enteredItems == 0)
+                    if (items.Count == 0)
                     {
                         EmptyDict();
                         return true;
@@ -93,7 +90,7 @@ namespace Inlämningsuppgift_2
                     CheckMin();
                     return true;
                 case 6:
-                    if (enteredItems == 0)
+                    if (items.Count == 0)
                     {
                         EmptyDict();
                         return true;
@@ -108,9 +105,114 @@ namespace Inlämningsuppgift_2
             }
         }
 
-        private static void AddItem()
+        private static void AddItems()
         {
-            throw new NotImplementedException();
+            int numberOfItems = AskForItems();
+            ConsoleKeyInfo input;
+
+            do
+            {
+                for (int i = 0; i < numberOfItems; i++)
+                {
+                    try
+                    {
+                    items.Add(ItemName(), ItemValue());
+                    }
+                    catch (ArgumentException)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("The name you entered already exists, please try again or use menu option 4. Change Item");
+                        PressAnyKey();
+                    }
+                }
+                do
+                {
+                    Console.WriteLine($"You have entered {items.Count} items so far");
+                    Console.WriteLine("Would you like to add more? Press y for yes and n for no");
+                    input = Console.ReadKey(true);
+                } while (input.Key != ConsoleKey.Y && input.Key != ConsoleKey.N);
+            } while (input.Key == ConsoleKey.Y);
+        }
+
+        private static string ItemName()
+        {
+            Console.WriteLine("Name:");
+            return Console.ReadLine();
+        }
+
+        private static int ItemValue()
+        {
+            int input = 0;
+            Console.WriteLine("Price:");
+            bool success = int.TryParse(Console.ReadLine(), NumberStyles.Integer, null, out input);
+
+            if (input < 1 || input > int.MaxValue)
+                do
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        if ((input < 1 && i < 3) || (input > int.MaxValue && i < 3))
+                        {
+                            Console.WriteLine("Please enter a valid number:");
+                            success = int.TryParse(Console.ReadLine(), NumberStyles.Integer, null, out input);
+                        }
+                        else if ((input < 1 && i == 3) || (input > int.MaxValue && i == 3))
+                        {
+                            Console.WriteLine($"A valid number is between 1 and {int.MaxValue}");
+                            Console.WriteLine("Please enter a valid number:");
+                            success = int.TryParse(Console.ReadLine(), NumberStyles.Integer, null, out input);
+                        }
+                        if (input >= 1 && input <= int.MaxValue) break;
+                    }
+                } while (input < 1 || input > int.MaxValue);
+
+            return input;
+        }
+
+        private static int AskForItems()
+        {
+            int entriesAmount = 0;
+            ConsoleKeyInfo input;
+
+            do
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (i == 0)
+                    {
+                        Console.WriteLine("How many items would you like to add?");
+                        input = Console.ReadKey(true);
+
+                        if (char.IsDigit(input.KeyChar))
+                        {
+                            entriesAmount = int.Parse(input.KeyChar.ToString());
+                        }
+                    }
+                    else if (i < 3)
+                    {
+                        Console.WriteLine("Please enter a valid number (between 1-9)");
+                        input = Console.ReadKey(true);
+
+                        if (char.IsDigit(input.KeyChar))
+                        {
+                            entriesAmount = int.Parse(input.KeyChar.ToString());
+                        }
+                    }
+                    else if (i == 3)
+                    {
+                        Console.WriteLine("Please press on a number between 1-9");
+                        input = Console.ReadKey(true);
+
+                        if (char.IsDigit(input.KeyChar))
+                        {
+                            entriesAmount = int.Parse(input.KeyChar.ToString());
+                        }
+                    }
+                    if (entriesAmount != 0) break;
+
+                }
+            } while (!(entriesAmount > 0 && entriesAmount < 10));
+            return entriesAmount;
         }
 
         private static void RemoveItem()
@@ -120,7 +222,14 @@ namespace Inlämningsuppgift_2
 
         private static void PrintDict()
         {
-            throw new NotImplementedException();
+            int i = 0;
+            foreach (KeyValuePair<string, int> kvp in items)
+            {
+                i++;
+                Console.Write($"{i}. ");
+                Console.WriteLine("{0}, {1}kr", kvp.Key, kvp.Value);
+            }
+            PressAnyKey();
         }
 
         private static void ChangeItem()
@@ -130,12 +239,32 @@ namespace Inlämningsuppgift_2
 
         private static void CheckMin()
         {
-            throw new NotImplementedException();
+            int minValue = items.Values.Min();
+
+            var matches = items.Where(pair => pair.Value == minValue)
+                  .Select(pair => pair.Key);
+            string[] keys = matches.ToArray();
+
+            if (keys.Length != 1)
+                Console.WriteLine($"The cheapest ingredients are {string.Join(", ", keys)} and costs {minValue}kr");
+            else Console.WriteLine($"The cheapest ingredient is {keys[0]} and costs {minValue}kr");
+
+            PressAnyKey();
         }
 
         private static void CheckMax()
         {
-            throw new NotImplementedException();
+            int maxValue = items.Values.Max();
+
+            var matches = items.Where(pair => pair.Value == maxValue)
+                  .Select(pair => pair.Key);
+            string[] keys = matches.ToArray();
+
+            if (keys.Length != 1)
+                Console.WriteLine($"The most expensive ingredients are {string.Join(", ", keys)} and costs {maxValue}kr");
+            else Console.WriteLine($"The most expensive ingredient is {keys[0]} and costs {maxValue}kr");
+
+            PressAnyKey();
         }
 
         private static void InputValidationChar()
